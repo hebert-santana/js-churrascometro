3a5b44bf72d3205fcf04603af6e55faa89302c64
Hebert Santana <102166830+hebert-santana@users.noreply.github.com>
2022-05-22 15:46:08 -0300

    edit cálculos acrescentar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 // Carne        - 150gr por pessoa por hora     + de 6 horas 150gr por pessoa por hora
 // Cerveja      - 500ml por pessoa por hora     + de 6 horas 450ml por pessoa por hora
 // Refrigerante - 400ml por pessoa por hora     + de 6 horas 250ml por pessoa por hora
-// criança (menor de 10 anos) consome equivalente à 0,5 pessoa
+// (Média) Adoslescente consome equivalente à 0,8 pessoa que não bebe.
+// (Média) Criança (menor de 12 anos) consome equivalente à 0,5 pessoa que não bebe.
 
 
 // criando variável (usando let) que vai receber o input
-let inputAdultos = document.getElementById("adultos");
+let inputAdultos_b = document.getElementById("adultos_bebem");
+let inputAdultos_nb = document.getElementById("adultos_naobebem");
+
+let inputAdolescente = document.getElementById("menorde18");
 let inputCriancas = document.getElementById("criancas");
+
 let inputDuracao = document.getElementById("duracao");
 
 // imprimindo as variáveis, importante determinar com ".value" pra buscar o valor
@@ -19,13 +24,16 @@ let resultado = document.getElementById("resultado");
 function calcular() {
     console.log("Calculando...");
 // determinar a quantidade de pessoas que serão inseridas .value
-    let adultos = inputAdultos.value;
+    let adultos_b = inputAdultos_b.value;
+    let adultos_nb = inputAdultos_nb.value;
+    let adolescente = inputAdolescente.value;
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
+
 // função duracao * número de adultos + função duracao/2 * número de crianças 
-    let Total_Carne = carnehoras(duracao) * adultos + (carnehoras(duracao)/2 * criancas);
-    let Total_Cerveja = cervejahoras(duracao) * adultos
-    let Total_Refri = refrihoras(duracao) * adultos + (refrihoras(duracao)/2) * criancas;
+    let Total_Carne = (carnehoras(duracao) * (adultos_b + adultos_nb)) + (carnehoras(duracao) * 0.8 * adolescente) + (carnehoras(duracao) * 0.4 * criancas);
+    let Total_Cerveja = cervejahoras(duracao) * adultos_b
+    let Total_Refri = (refrihoras(duracao) * adultos_nb) + (refrihoras(duracao) * 0.8 * adolescente) + (refrihoras(duracao) * 0.5 * criancas);
 
 
 // PRIMEIRA FRASE //
@@ -37,8 +45,8 @@ function calcular() {
     
 // += para não substituir o que já está em innerHTML
 // usar função pra pegar o maior inteiro após a divisão
-    resultado.innerHTML += `<strong> <p>${Math.ceil(Total_Cerveja / 355)} latas de cerveja.</p> </strong>`    
-    resultado.innerHTML += `<strong> <p>${Math.ceil(Total_Refri / 2000)} garrafas de 2 litros de refrigerante.</p> </strong>`
+    resultado.innerHTML += `<strong> <p>${Math.ceil((Total_Cerveja) / 355)} latas de cerveja.</p> </strong>`    
+    resultado.innerHTML += `<strong> <p>${Math.ceil((Total_Refri) / 2000)} garrafas de 2 litros de refrigerante.</p> </strong>`
     resultado.innerHTML += `<center> <p>BOA FESTA!</p> </center>`
     
 
@@ -47,9 +55,9 @@ function calcular() {
 
 // função carne em relação as horas
 
-function carnehoras(duracao) {          // consumo médio sempre de 150g/h //
-    if (duracao >= 6){
-        return 150*duracao;
+function carnehoras(duracao) {          // consumo médio sempre de 150g/h até 5 horas de duração
+    if (duracao >= 5){
+        return 120*(duracao - 5) + 160*5;
     }
     else {
         return 150*duracao;
@@ -59,19 +67,19 @@ function carnehoras(duracao) {          // consumo médio sempre de 150g/h //
 // função cerveja em relação as horas
 
 function cervejahoras(duracao) {
-    if (duracao >= 6){
-        return 450*duracao;
+    if (duracao >= 5){
+        return ((450*(duracao - 5)) + 600*5)  ; // 600*5 das 5 primeiras horas + 450 ml por hora
     }
     else {
-        return 500*duracao;
+        return 600*duracao;
     }
 }
 
 // função refrigerante em relação as horas
 
 function refrihoras(duracao) {
-    if (duracao >= 6){
-        return 250*duracao;
+    if (duracao >= 5){
+        return (( 250*(duracao - 5)) + 400*5);
     }
     else {
         return 400*duracao;
